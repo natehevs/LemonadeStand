848cda96fcce667d2acdb9f93fddd006c0a7ae87
patrick hevrdejs <nathanhevrdejs@gmail.com>
2019-09-30 09:32:29 -0500

    added all classes that are nexessary for now

---
+++ LemonadeStand/LemonadeStand/Pitcher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class Pitcher
+    {
+        // member variables (HAS A)
+
+
+
+        // constructor
+
+
+
+        // member methods (CAN DO)
+    }
+}
---
+++ LemonadeStand/LemonadeStand/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // member variables (HAS A)
+
+
+
+            // constructor
+
+
+
+            // member methods (CAN DO)
+        }
+    }
+}
