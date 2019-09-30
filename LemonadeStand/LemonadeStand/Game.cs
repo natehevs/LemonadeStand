230cb5d32f9191ad1f8dcd2c677fe31bfc706e35
patrick hevrdejs <nathanhevrdejs@gmail.com>
2019-09-30 10:36:27 -0500

    made my outline/steps im going to do to run the game and have it outlined

--- LemonadeStand/LemonadeStand/Game.cs
+++ LemonadeStand/LemonadeStand/Game.cs
@@ -8,6 +8,37 @@ namespace LemonadeStand
 {
     class Game
     {
+        /*
+         * 1. display information
+               a. display instructions/ explanations
+        3. get name
+        4. show current lemons, sugar, and ice which would be 0 at beginning
+            a. display weather for today
+        5. prompt to shop for ingredients
+            a. what price would you like to sell lemonade at?
+            b. how many sugar would you like to buy
+                HAVE 3 OPTIONS
+            c. how many lemons would you like to buy
+                HAVE 3 OPTIONS
+            d. how much ice would you like to buy
+                HAVE 3 OPTIONS
+            e. how many cups would you like to buy
+                HAVE 3 OPTIONS
+        6. ask for recipe
+            a. how many lemons, sugar, and ice per cup.
+        7. start day
+            a. display weather, and have designated prompt saying how well or bad sales are based on the day
+        8. Sell until day is over/all designated sales are made based on predetermined weather conditions
+            a. if they didnt have enough resources stop day early and display "sold out"
+        9. display day results
+            a. profit
+            b. resources after day
+            c. how many cups sold
+            d. refreshment %
+            e. if ice is left over melt all of it
+        10. go back to step 4
+        */
+
         // member variables (HAS A)
 
 
@@ -17,5 +48,9 @@ namespace LemonadeStand
 
 
         // member methods (CAN DO)
+        internal void RunGame()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
